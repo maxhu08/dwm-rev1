3e79f9713add0129a52e33f4c8fcb7fb3d9ca6d0
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-06-29 15:04:01 -0500

    add theme

--- config.h
+++ config.h
@@ -13,15 +13,11 @@ static const int showbar            = 1;        // 0 means no bar
 static const int topbar             = 1;        // 0 means bottom bar
 static const char *fonts[]          = { "Jetbrains Mono NerdFont:size=12:style=Bold", "monospace:size=12" };
 static const char dmenufont[]       = "Jetbrains Mono NerdFont:size=12:style=Bold";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+#include "themes/mhtheme.h"
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_accnt, col_accnt  },
 };
 
 /* tagging */
@@ -84,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accnt, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 #include "movestack.c"
---
+++ themes/mhtheme.h
@@ -0,0 +1,8 @@
+// mh's them
+// clang-format off
+
+static const char col_gray1[] = "#0a0a0a";
+static const char col_gray2[] = "#171717";
+static const char col_gray3[] = "#cdd6f4";
+static const char col_gray4[] = "#0a0a0a";
+static const char col_accnt[] = "#06b6d4";
