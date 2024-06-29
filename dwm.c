8bb43ee5cd82b7ba7253e97eec67010ba18b5a46
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-06-29 16:57:08 -0500

    make dragging and resizing windows a lot smoother

--- dwm.c
+++ dwm.c
@@ -1177,7 +1177,7 @@ void movemouse(const Arg *arg) {
       handler[ev.type](&ev);
       break;
     case MotionNotify:
-      if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+      if ((ev.xmotion.time - lasttime) <= (1000 / 240))
         continue;
       lasttime = ev.xmotion.time;
 
@@ -1370,7 +1370,7 @@ void resizemouse(const Arg *arg) {
       handler[ev.type](&ev);
       break;
     case MotionNotify:
-      if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+      if ((ev.xmotion.time - lasttime) <= (1000 / 240))
         continue;
       lasttime = ev.xmotion.time;
 
