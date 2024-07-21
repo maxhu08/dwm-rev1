2b20306d009f7b8a0be248c48101e298266b2924
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-21 17:59:53 -0500

    change border color

--- themes/mhtheme.h
+++ themes/mhtheme.h
@@ -2,7 +2,7 @@
 // clang-format off
 
 static const char col_gray1[] = "#0a0a0a";
-static const char col_gray2[] = "#171717";
+static const char col_gray2[] = "#262626";
 static const char col_gray3[] = "#cdd6f4";
 static const char col_gray4[] = "#0a0a0a";
 static const char col_accnt[] = "#99f6e4";
