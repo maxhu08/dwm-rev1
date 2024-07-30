945b7d76b17441c346a012697f6ff2feb1027ce9
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-30 14:28:31 -0500

    update screenshot script

--- config.h
+++ config.h
@@ -84,8 +84,8 @@ static const Key keys[] = {
 	/* modifier                     key                    function        argument */
 	{ MODKEY,                       XK_r,                  spawn,          {.v = dmenucmd } },                                // open app launcher MOD+r
 	{ MODKEY,                       XK_Return,             spawn,          {.v = termcmd } },                                 // spawn terminal MOD+return
-	{ MODKEY,                       XK_s,                  spawn,          SHCMD("exec ~/.hidden/scripts/screenshot.sh") },       // screenshot MOD+s (requires script in ~/Dev/scripts/screenshot.sh)
-	{ MODKEY|ShiftMask,             XK_s,                  spawn,          SHCMD("exec ~/.hidden/scripts/screenshot_crop.sh") },  // screenshot MOD+S (requires script in ~/Dev/scripts/screenshot_crop.sh)
+	{ MODKEY,                       XK_s,                  spawn,          SHCMD("exec ~/.hidden/scripts/screenshot_multimon.sh") },       // screenshot MOD+s (requires script in ~/Dev/scripts/screenshot.sh)
+	{ MODKEY|ShiftMask,             XK_s,                  spawn,          SHCMD("exec ~/.hidden/scripts/screenshot_crop.sh") },           // screenshot MOD+S (requires script in ~/Dev/scripts/screenshot_crop.sh)
 	{ MODKEY,                       XK_e,                  spawn,          SHCMD("exec nemo") },                              // open file manager (nemo) MOD+e
 	{ MODKEY,                       XK_b,                  togglebar,      {0} },                                             // show hide bar MOD+b
 	{ MODKEY|ALTKEY|ShiftMask,      XK_l,                  spawn,          SHCMD("exec slock") },                             // lockscreen MOD+ALT+L
